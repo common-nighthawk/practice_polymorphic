704e97d0fa4edb7c3f9dd28216071ae66584409c
Daniel <danielallendeutsch@gmail.com>
2014-07-28 13:58:53 -0400

    tests for views and models

--- app/models/golf_post.rb
+++ app/models/golf_post.rb
@@ -1,3 +1,6 @@
 class GolfPost < ActiveRecord::Base
+	validates :golf_title, presence: true
+	validates :golf_article, presence: true
+
 	has_many :comments, :as => :commentable
 end
--- spec/models/golf_post_spec.rb
+++ spec/models/golf_post_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe GolfPost do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "both title and article must be present" do
+
+  	it "creates an object a title and article" do
+  		@golf_post = GolfPost.new(golf_title: "hello 1", golf_article: "hello 2")
+			@golf_post.should be_valid
+  	end
+
+  	it "does not create an object without a title" do
+  		@golf_post = GolfPost.new(golf_article: "hello 2")
+			@golf_post.should_not be_valid
+  	end
+
+  	it "does not create an object without an article" do
+  		@golf_post = GolfPost.new(golf_title: "hello 1")
+			@golf_post.should_not be_valid
+  	end
+
+  end
 end
